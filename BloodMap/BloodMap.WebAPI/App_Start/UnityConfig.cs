03c23db93224244b8417d03490545e6e8c75281c
unknown <sagar.raut@sraut-L01.jadeglobal.com>
2016-03-15 14:08:10 +0530

    DI changes

--- BloodMap/BloodMap.WebAPI/App_Start/UnityConfig.cs
+++ BloodMap/BloodMap.WebAPI/App_Start/UnityConfig.cs
@@ -9,12 +9,18 @@ namespace BloodMap.WebAPI
         public static void RegisterComponents()
         {
 			var container = new UnityContainer();
-            
+
             // register all your components with the container here
             // it is NOT necessary to register your controllers
-            
-            // e.g. container.RegisterType<ITestService, TestService>();
-            
+
+            //container.RegisterType<IBloodGroupService, BloodGroupService>();
+
+            container.RegisterTypes(
+               AllClasses.FromLoadedAssemblies(),
+               WithMappings.FromMatchingInterface,
+               WithName.Default,
+               WithLifetime.ContainerControlled);
+
             GlobalConfiguration.Configuration.DependencyResolver = new UnityDependencyResolver(container);
         }
     }
