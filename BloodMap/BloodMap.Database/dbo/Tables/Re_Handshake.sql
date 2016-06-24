0c2e2e0cb19d4541fbdb3c95ee920d6f4d9a1c9c
Reishabh Saxena <reishabh@gmail.com>
2016-06-24 11:59:59 +0530

    Db Changes published

---
+++ BloodMap/BloodMap.Database/dbo/Tables/Re_Handshake.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Re_Handshake] (
+    [Id]               UNIQUEIDENTIFIER CONSTRAINT [DF_Re_Handshake_Id] DEFAULT (newid()) NOT NULL,
+    [SeriesIdentifier] VARCHAR (100)    NULL,
+    [Token]            VARCHAR (500)    NULL,
+    [CreatedDate]      DATETIME         NOT NULL,
+    [LastLogin]        DATETIME         NULL,
+    [UserId]           INT              NOT NULL,
+    CONSTRAINT [PK_Re_Handshake] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_Re_Handshake_User] FOREIGN KEY ([UserId]) REFERENCES [dbo].[User] ([UserId])
+);
+
--- BloodMap/BloodMap.Database/dbo/Tables/User.sql
+++ BloodMap/BloodMap.Database/dbo/Tables/User.sql
@@ -10,11 +10,13 @@
     [ModifiedDate] DATETIME       NULL,
     [EmailId]      NVARCHAR (250) NULL,
     [Password]     NVARCHAR (50)  NULL,
-    [DonorId]      INT            NULL,
+    [DonorId]      INT            NOT NULL,
     CONSTRAINT [PK_User] PRIMARY KEY CLUSTERED ([UserId] ASC),
     CONSTRAINT [FK_User_Donor] FOREIGN KEY ([DonorId]) REFERENCES [dbo].[Donor] ([DonorId]),
     CONSTRAINT [FK_User_L_Role] FOREIGN KEY ([L_RoleId]) REFERENCES [dbo].[L_Role] ([L_RoleId])
 );
+
+
 
 
 
